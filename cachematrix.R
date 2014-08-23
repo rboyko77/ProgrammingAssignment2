de5ebc23416fe8447c1badf4535c9868cb94a578
Robert Boyko <rbo@iMac-2.local>
2014-08-22 23:35:33 -0600

    nicer to read comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## 
 ## These functions work in tandem to return the inverse of a matrix "x".
+## The matrix is assumed to be invertible.
 ##
 ## The cover function checks to see if the inverse has already been cached
 ## before calling solve(x) to do the work. The cpu savings can be 
@@ -9,16 +10,17 @@
 ## Example Usage:
 ##
 ##      # Setup
-##		x = matrix(c(1,2,3,4), nrow=2, ncol=2, byrow=TRUE)
-##		a = makeCacheMatrix(x)
+##      x <- matrix(c(1,2,3,4), nrow=2, ncol=2, byrow=TRUE)
+##      a <- makeCacheMatrix(x)
 ##
-##		# Invoke (first time calculated, second time retrieved from cache)
-##		cacheSolve(a)
-##			Calculating inverse...
-##		cacheSolve(a)
-##			Getting cached data...			
+##      # Invoke (first time calculated, second time retrieved from cache)
+##      cacheSolve(a)
+##          Calculating inverse...
+##      cacheSolve(a)
+##          Getting cached data...   
 ##
 ########################################################################
+
 
 ##
 ## Find the inverse of a matrix "x" and cache the result.
@@ -30,26 +32,26 @@
 ##
 makeCacheMatrix <- function(x = matrix()) {
 
-		# set data matrix
+        # set data matrix
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
 
-		# get data matrix
+        # get data matrix
         get <- function() x
 
-		# set and cache the inverse matrix 
+        # set and cache the inverse matrix 
         setsolve <- function(solve) m <<- solve 
 
-		# retrieve the input matrix
+        # retrieve the input matrix
         getsolve <- function() m
 
-		# return operations vector
+        # return operations vector
         list(set = set, get = get,
-             setsolve = setsolve,
-             getsolve = getsolve)
+            setsolve = setsolve,
+            getsolve = getsolve)
 
 }
 
@@ -58,19 +60,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ##
 cacheSolve <- function(x, ...) {
 
-		# check if we have the inverse cached, return it and exit. 
-        m <- x$getsolve()
-        if(!is.null(m)) {
-                message("  Getting cached data...")
-                return(m)
-		}
+    # check if we have the inverse cached
+    m <- x$getsolve()
+    if(!is.null(m)) {
 
-		# otherwise we calculate it calling solve().
-        message("  Calculating inverse...")
-        data <- x$get()
-        m <- solve(data, ...)
+        # and make sure the matrix has not changed
+        message("  Getting cached data...")
+        return(m)
+    }
 
-		# cache it before exiting.
-        x$setsolve(m)
-        m
+    # otherwise we calculate it calling solve().
+    message("  Calculating inverse...")
+    data <- x$get()
+    m <- solve(data, ...)
+
+    # cache it before exiting.
+    x$setsolve(m)
+    m
 }
